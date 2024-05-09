b23bf8a5efa2758414ed81cef2f3293420d2691d
yannannan <18810994501@163.com>
2024-05-09 14:50:40 +0800

    chore: 存档&读档ui

--- packages/webgal/src/UI/Menu/SaveAndLoad/Load/Load.tsx
+++ packages/webgal/src/UI/Menu/SaveAndLoad/Load/Load.tsx
@@ -1,15 +1,14 @@
-import { CSSProperties, FC, useEffect } from 'react';
+import { FC, useEffect } from 'react';
 import { loadGame } from '@/Core/controller/storage/loadGame';
 import styles from '../SaveAndLoad.module.scss';
-// import {saveGame} from '@/Core/controller/storage/saveGame';
 import { setStorage } from '@/Core/controller/storage/storageController';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 import { setSlPage } from '@/store/userDataReducer';
 import useTrans from '@/hooks/useTrans';
-import { useTranslation } from 'react-i18next';
 import useSoundEffect from '@/hooks/useSoundEffect';
 import { getSavesFromStorage } from '@/Core/controller/storage/savesController';
+import { setVisibility } from '@/store/GUIReducer';
 
 export const Load: FC = () => {
   const { playSeClick, playSeEnter, playSePageChange } = useSoundEffect();
@@ -17,10 +16,10 @@ export const Load: FC = () => {
   const saveDataState = useSelector((state: RootState) => state.saveData);
   const dispatch = useDispatch();
   const page = [];
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 1; i <= 4; i++) {
     let classNameOfElement = styles.Save_Load_top_button + ' ' + styles.Load_top_button;
     if (i === userDataState.optionData.slPage) {
-      classNameOfElement = classNameOfElement + ' ' + styles.Save_Load_top_button_on + ' ' + styles.Load_top_button_on;
+      classNameOfElement = classNameOfElement + ' ' + styles.Save_Load_top_button_on;
     }
     const element = (
       <div
@@ -33,16 +32,16 @@ export const Load: FC = () => {
         key={'Load_element_page' + i}
         className={classNameOfElement}
       >
-        <div className={styles.Save_Load_top_button_text}>{i}</div>
+        <div className={styles.Save_Load_top_button_text} />
       </div>
     );
     page.push(element);
   }
 
   const showSaves = [];
-  // 现在尝试设置10个存档每页
-  const start = (userDataState.optionData.slPage - 1) * 10 + 1;
-  const end = start + 9;
+  // 现在尝试设置6个存档每页
+  const start = (userDataState.optionData.slPage - 1) * 6 + 1;
+  const end = start + 5;
 
   useEffect(() => {
     getSavesFromStorage(start, end);
@@ -52,32 +51,36 @@ export const Load: FC = () => {
   for (let i = start; i <= end; i++) {
     animationIndex++;
     const saveData = saveDataState.saveData[i];
-    let saveElementContent = <div />;
+    let saveElementContent = <div className={styles.Save_Load_content_space} />;
     if (saveData) {
       const speaker = saveData.nowStageState.showName === '' ? '\u00A0' : `${saveData.nowStageState.showName}`;
       saveElementContent = (
         <>
-          <div className={styles.Save_Load_content_element_top}>
+          {/* <div className={styles.Save_Load_content_element_top}>
             <div className={styles.Save_Load_content_element_top_index + ' ' + styles.Load_content_elememt_top_index}>
               {saveData.index}
             </div>
             <div className={styles.Save_Load_content_element_top_date + ' ' + styles.Load_content_element_top_date}>
               {saveData.saveTime}
             </div>
-          </div>
+          </div> */}
+          <div className={styles.Save_Load_border} />
           <div className={styles.Save_Load_content_miniRen}>
             <img className={styles.Save_Load_content_miniRen_bg} alt="Save_img_preview" src={saveData.previewImage} />
           </div>
-          <div className={styles.Save_Load_content_text}>
+          {/* <div className={styles.Save_Load_content_text}>
             <div className={styles.Save_Load_content_speaker + ' ' + styles.Load_content_speaker}>{speaker}</div>
             <div className={styles.Save_Load_content_text_padding}>{saveData.nowStageState.showText}</div>
+          </div> */}
+          <div className={styles.Save_Load_info}>
+            <div className={styles.Save_Load_info_box}>
+              <div className={styles.Save_Load_content_element_top_date}>{saveData.saveTime}</div>
+              <div className={styles.Save_Load_content_element_top_date}>{saveData.nowStageState.showText}</div>
+            </div>
           </div>
         </>
       );
     }
-    // else {
-
-    // }
     const saveElement = (
       <div
         onClick={() => {
@@ -97,16 +100,37 @@ export const Load: FC = () => {
 
   const t = useTrans('menu.');
 
+  const handleBtnClick = (dir: string) => {
+    const slPage = userDataState.optionData.slPage;
+    let index = slPage;
+    if (dir === 'left') {
+      index = index > 1 ? index - 1 : 4;
+    } else {
+      index = index < 4 ? index + 1 : 1;
+    }
+    dispatch(setSlPage(index));
+    setStorage();
+    playSePageChange();
+  };
+
   return (
     <div className={styles.Save_Load_main}>
       <div className={styles.Save_Load_top}>
-        <div className={styles.Save_Load_title}>
-          <div className={styles.Load_title_text}>{t('loadSaving.title')}</div>
-        </div>
-        <div className={styles.Save_Load_top_buttonList}>{page}</div>
+        <div className={styles.Save_back} />
+        <div
+          className={styles.Load_title}
+          onClick={() => {
+            dispatch(setVisibility({ component: 'showMenuPanel', visibility: false }));
+          }}
+        />
       </div>
       <div className={styles.Save_Load_content} id={'Load_content_page_' + userDataState.optionData.slPage}>
         {showSaves}
+      </div>
+      <div className={styles.Save_Load_top_buttonList}>
+        <div className={styles.Btn_l} onClick={() => handleBtnClick('left')} />
+        {page}
+        <div className={styles.Btn_r} onClick={() => handleBtnClick('right')} />
       </div>
     </div>
   );
