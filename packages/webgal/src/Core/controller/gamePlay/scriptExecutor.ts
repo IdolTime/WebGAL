6d1e4a64e4a6842b76003a2674f1ceac8305d934
hermine <hermine0313@gmail.com>
2024-05-27 10:29:42 +0800

    fix: 恢复调试隐藏

--- packages/webgal/src/Core/controller/gamePlay/scriptExecutor.ts
+++ packages/webgal/src/Core/controller/gamePlay/scriptExecutor.ts
@@ -55,14 +55,14 @@ export const scriptExecutor = () => {
 
   const interpolationOneItem = (content: string): string => {
     let retContent = content;
-    // const contentExp = retContent.match(/(?<!\\)\{(.*?)\}/g);
+    const contentExp = retContent.match(/(?<!\\)\{(.*?)\}/g);
 
-    // if (contentExp !== null) {
-    //   contentExp.forEach((e) => {
-    //     const contentVarValue = getValueFromState(e.replace(/(?<!\\)\{(.*)\}/, '$1'));
-    //     retContent = retContent.replace(e, contentVarValue ? contentVarValue.toString() : e);
-    //   });
-    // }
+    if (contentExp !== null) {
+      contentExp.forEach((e) => {
+        const contentVarValue = getValueFromState(e.replace(/(?<!\\)\{(.*)\}/, '$1'));
+        retContent = retContent.replace(e, contentVarValue ? contentVarValue.toString() : e);
+      });
+    }
     retContent = retContent.replace(/\\{/g, '{').replace(/\\}/g, '}');
     return retContent;
   };
