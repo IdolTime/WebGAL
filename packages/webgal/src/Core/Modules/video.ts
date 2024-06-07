b882e95943b0650f7381b3cc4e7109a130c557be
alan walker <razor1895@gmail.com>
2024-06-07 12:40:22 +0800

    chore: fix ios issue

--- packages/webgal/src/Core/Modules/video.ts
+++ packages/webgal/src/Core/Modules/video.ts
@@ -1,11 +1,29 @@
 import FlvJs from 'flv.js';
+// @ts-ignore
+import CryptoJS from 'crypto-js';
+
+function arrayBufferToWordArray(arrayBuffer: ArrayBuffer) {
+  const uint8Array = new Uint8Array(arrayBuffer);
+  const words = [];
+  for (let i = 0; i < uint8Array.length; i += 4) {
+    words.push((uint8Array[i] << 24) | (uint8Array[i + 1] << 16) | (uint8Array[i + 2] << 8) | uint8Array[i + 3]);
+  }
+  return CryptoJS.lib.WordArray.create(words, uint8Array.length);
+}
 
 // eslint-disable-next-line max-params
-async function decryptVideo(encryptedData: ArrayBuffer, key: Uint8Array, iv: Uint8Array, type: 'mp4' | 'flv') {
-  const algorithm = { name: 'AES-CBC', iv: iv };
-  const cryptoKey = await crypto.subtle.importKey('raw', key, algorithm, false, ['decrypt']);
-  const decryptedData = await crypto.subtle.decrypt(algorithm, cryptoKey, encryptedData);
-  return new Blob([new Uint8Array(decryptedData)], { type: `video/${type === 'mp4' ? 'mp4' : 'x-flv'}` });
+async function decryptVideo(encryptedData: ArrayBuffer, key: string, iv: string, type: 'mp4' | 'flv') {
+  const keyHex = CryptoJS.enc.Hex.parse(key);
+  const ivHex = CryptoJS.enc.Hex.parse(iv);
+  const encryptedWordArray = arrayBufferToWordArray(encryptedData);
+  const decrypted = CryptoJS.AES.decrypt({ ciphertext: encryptedWordArray }, keyHex, { iv: ivHex });
+
+  const decryptedData = new Uint8Array(decrypted.sigBytes);
+  for (let i = 0; i < decrypted.sigBytes; i++) {
+    decryptedData[i] = (decrypted.words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+  }
+
+  return new Blob([decryptedData], { type: `video/${type === 'mp4' ? 'mp4' : 'x-flv'}` });
 }
 
 export class VideoManager {
@@ -123,13 +141,8 @@ export class VideoManager {
         let videoBlob;
         if (isEncrypted) {
           const encryptedData = dataBuffer.slice(markerLength);
-          const key = new Uint8Array([
-            0x40, 0xe6, 0xad, 0x42, 0x9a, 0x13, 0x02, 0x0a, 0x07, 0xbe, 0x29, 0x0c, 0x5e, 0xf1, 0xd7, 0xdc, 0x7e, 0x45,
-            0xe5, 0xc4, 0xbf, 0x34, 0xd5, 0x4a, 0x56, 0x64, 0x28, 0x26, 0x27, 0x94, 0x6e, 0x4d,
-          ]);
-          const iv = new Uint8Array([
-            0x9d, 0x6b, 0xac, 0x74, 0xc6, 0x4e, 0xe8, 0x71, 0x4e, 0x79, 0x59, 0xce, 0xf7, 0x52, 0x71, 0xd0,
-          ]);
+          const key = '40e6ad429a13020a07be290c5ef1d7dc7e45e5c4bf34d54a5664282627946e4d';
+          const iv = '9d6bac74c64ee8714e7959cef75271d0';
           videoBlob = await decryptVideo(encryptedData, key, iv, videoType);
         } else {
           videoBlob = new Blob([new Uint8Array(dataBuffer)], {
