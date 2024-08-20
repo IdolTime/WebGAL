ca8a6bfdf7368440cf48667050c091bb19b10c69
alan walker <razor1895@gmail.com>
2024-08-20 15:43:00 +0800

    chore: update parser

--- packages/parser/src/config/scriptConfig.ts
+++ packages/parser/src/config/scriptConfig.ts
@@ -34,6 +34,9 @@ export const SCRIPT_CONFIG = [
   { scriptString: 'unlockAchieve', scriptType: commandType.unlockAchieve },
   { scriptString: 'unlockStoryline', scriptType: commandType.unlockStoryline },
   { scriptString: 'showValue', scriptType: commandType.showValue },
+  { scriptString: 'payProduct', scriptType: commandType.payProduct },
+  { scriptString: 'finishTrial', scriptType: commandType.finishTrial },
+  { scriptString: 'moveCamera', scriptType: commandType.moveCamera },
 ];
 export const ADD_NEXT_ARG_LIST = [
   commandType.bgm,
