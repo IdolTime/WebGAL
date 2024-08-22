6d2b3637184b9b915bf00b55c72584c4ecf7ae86
alan walker <razor1895@gmail.com>
2024-08-22 18:21:03 +0800

    chore: 播放音效

--- packages/webgal/src/Core/gameScripts/changeAffinity.ts
+++ packages/webgal/src/Core/gameScripts/changeAffinity.ts
@@ -1,8 +1,10 @@
-import { ISentence } from '@/Core/controller/scene/sceneInterface';
+import { commandType, ISentence } from '@/Core/controller/scene/sceneInterface';
 import { IPerform } from '@/Core/Modules/perform/performInterface';
 import { webgalStore } from '@/store/store';
 import { addShowAffinityChangeList, setStage, updateShowAffinityChangeList } from '@/store/stageReducer';
 import { assetSetter, fileType } from '../util/gameAssetsAccess/assetSetter';
+import { getRandomPerformName } from '../Modules/perform/performController';
+import { playEffect } from './playEffect';
 
 /**
  * 修改好感度
@@ -13,10 +15,13 @@ export const changeAffinity = (sentence: ISentence): IPerform => {
   let roleUrl = assetSetter(sentence.content, fileType.ui);
   let numberUrl = '';
   let key = Date.now();
+  let soundUrl = '';
 
   sentence.args.forEach((e) => {
     if (e.key === 'number') {
       numberUrl = assetSetter(e.value.toString().trim(), fileType.ui);
+    } else if (e.key === 'sound' && e.value) {
+      soundUrl = assetSetter(e.value.toString().trim(), fileType.vocal);
     }
   });
 
@@ -28,8 +33,19 @@ export const changeAffinity = (sentence: ISentence): IPerform => {
     }),
   );
 
+  if (soundUrl) {
+    playEffect({
+      command: commandType.playEffect,
+      commandRaw: 'playEffect',
+      content: soundUrl,
+      sentenceAssets: [],
+      subScene: [],
+      args: [],
+    });
+  }
+
   return {
-    performName: 'none',
+    performName: getRandomPerformName(),
     duration: 0,
     isHoldOn: false,
     stopFunction: () => {
