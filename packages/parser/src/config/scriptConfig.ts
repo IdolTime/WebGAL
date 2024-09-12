250071eee7bf3ff63f1d97dcb461e745fbd61f0b
alan walker <razor1895@gmail.com>
2024-09-12 19:21:45 +0800

    chore: 增加clearScreen command

--- packages/parser/src/config/scriptConfig.ts
+++ packages/parser/src/config/scriptConfig.ts
@@ -40,6 +40,7 @@ export const SCRIPT_CONFIG = [
   { scriptString: 'changeAffinity', scriptType: commandType.changeAffinity },
   { scriptString: 'unlockAffinity', scriptType: commandType.unlockAffinity },
   { scriptString: 'popUpImage', scriptType: commandType.popUpImage },
+  { scriptString: 'clearScreen', scriptType: commandType.clearScreen },
 ];
 export const ADD_NEXT_ARG_LIST = [
   commandType.bgm,
