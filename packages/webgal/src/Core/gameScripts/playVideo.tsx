16b39f7936f22a48ee9271cfbb522bf033f2047f
hermine <hermine0313@gmail.com>
2024-09-23 13:51:22 +0800

    fix: 开启/关闭持续播放视频时未清除当前文本

--- packages/webgal/src/Core/gameScripts/playVideo.tsx
+++ packages/webgal/src/Core/gameScripts/playVideo.tsx
@@ -10,7 +10,7 @@ import { sceneParser } from '../parser/sceneParser';
 import { scenePrefetcher } from '@/Core/util/prefetcher/scenePrefetcher';
 import { getCurrentVideoStageDataForStoryLine } from '@/Core/controller/storage/saveGame';
 import { setshowFavorited, setVisibility } from '@/store/GUIReducer';
-import { updateShowValueList } from '@/store/stageReducer';
+import { updateShowValueList, setStage } from '@/store/stageReducer';
 
 /**
  * 播放一段视频 * @param sentence
@@ -29,6 +29,9 @@ export const playVideo = (sentence: ISentence): IPerform => {
   const optionId = Date.now();
   webgalStore.dispatch(setshowFavorited(false));
   webgalStore.dispatch(setVisibility({ component: 'playingVideo', visibility: true }));
+  webgalStore.dispatch(setStage({ key: 'showText', value: '' }));
+  webgalStore.dispatch(setStage({ key: 'showName', value: '' }));
+  webgalStore.dispatch(setVisibility({ component: 'showTextBox', visibility: false }));
   const endPerformRef = {
     current: () => {
       console.log('快进状态尝试跳过视频');
