23d094ad29b2a20c835067bc3471c09f867fe776
alan walker <razor1895@gmail.com>
2024-10-10 18:26:30 +0800

    chore: 视频鉴赏

--- packages/webgal/src/UI/Extra/ExtraCgElement.tsx
+++ packages/webgal/src/UI/Extra/ExtraCgElement.tsx
@@ -3,15 +3,29 @@ import styles from '@/UI/Extra/extra.module.scss';
 import React from 'react';
 import useSoundEffect from '@/hooks/useSoundEffect';
 import cgUnLock from '@/assets/imgs//cg-unLock.png';
+import FlvPlayer from '../FlvPlayer';
+import FlvJs from 'flv.js';
+import { assetSetter, fileType } from '@/Core/util/gameAssetsAccess/assetSetter';
 
 interface IProps {
   name: string;
-  imgUrl: string;
+  url: string;
 }
 
 export function ExtraCgElement(props: IProps) {
   const showFull = useValue(false);
   const { playSeEnter, playSeClick } = useSoundEffect();
+  const isVideo = props.url.endsWith('.mp4') || props.url.endsWith('.flv');
+  const videoPlayerRef = React.useRef<FlvJs.FlvPlayer>(null);
+  const bgmNode = document.getElementById('currentBgm') as HTMLAudioElement;
+  const url = assetSetter(props.url, isVideo ? fileType.video : fileType.background);
+
+  let bgmVol = 0;
+
+  if (bgmNode) {
+    bgmVol = bgmNode.volume;
+  }
+
   return (
     <>
       {showFull.value && (
@@ -24,15 +38,26 @@ export function ExtraCgElement(props: IProps) {
           onMouseEnter={playSeEnter}
         >
           <div className={styles.showFullCgMain}>
-            <div
-              style={{
-                backgroundImage: `url('${props.imgUrl}')`,
-                backgroundSize: `cover`,
-                backgroundPosition: 'center',
-                width: '100%',
-                height: '100%',
-              }}
-            />
+            {isVideo ? (
+              <FlvPlayer
+                videoPlayerRef={videoPlayerRef}
+                url={url}
+                controls
+                autoPlay={false}
+                muted
+                key={url + props.name}
+              />
+            ) : (
+              <div
+                style={{
+                  backgroundImage: `url('${url}')`,
+                  backgroundSize: `cover`,
+                  backgroundPosition: 'center',
+                  width: '100%',
+                  height: '100%',
+                }}
+              />
+            )}
           </div>
         </div>
       )}
@@ -45,13 +70,27 @@ export function ExtraCgElement(props: IProps) {
         key={props.name}
         className={`${styles.cgElement} ${styles.cgUnLockElement} interactive`}
       >
-        <div
-          className={styles.unlockedCGImg}
-          style={{
-            background: `url('${props.imgUrl}') no-repeat center center`,
-            backgroundSize: `cover`,
-          }}
-        />
+        {isVideo ? (
+          <div className={`${styles.videoContainer} ${styles.unlockedCGImg}`}>
+            <FlvPlayer
+              videoPlayerRef={videoPlayerRef}
+              url={url}
+              controls={false}
+              autoPlay={false}
+              muted
+              key={url + props.name}
+            />
+            <span className={styles.playButtonIcon} />
+          </div>
+        ) : (
+          <div
+            className={styles.unlockedCGImg}
+            style={{
+              background: `url('${url}') no-repeat center center`,
+              backgroundSize: `cover`,
+            }}
+          />
+        )}
       </div>
     </>
   );
