8882f7a29d08cfd24bc5330a184e874408be57d7
hermine <hermine0313@gmail.com>
2024-10-11 10:13:48 +0800

    fix: 埋点接口变更

--- packages/webgal/src/utils/trackEvent.ts
+++ packages/webgal/src/utils/trackEvent.ts
@@ -9,14 +9,19 @@ let timer: NodeJS.Timer | undefined;
 // 启动定时器，每2分钟请求一次数据
 export function startEvent() {
 
-      function requestData() {
-        const params = {
-            thirdUserId: sessionStorage.getItem('sdk-userId') as string,
-            productId: WebGAL.gameId + '',
-            reportTime: getLocalDate()
-          }
-          apiUserOnlineLogEvent(params)
-      }
+    if (!timer) {
+      clearInterval(timer);
+      requestData()
+    }
+
+    function requestData() {
+      const params = {
+          thirdUserId: sessionStorage.getItem('sdk-userId') as string,
+          productId: WebGAL.gameId + '',
+          reportTime: getLocalDate()
+        }
+        apiUserOnlineLogEvent(params)
+    }
       
     timer = setInterval(requestData, 2 * 60 * 1000); // 2分钟 = 2 * 60 * 1000 毫秒
 }
